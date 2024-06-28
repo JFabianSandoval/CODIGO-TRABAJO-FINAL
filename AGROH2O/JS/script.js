fba5dae8b00c3a5bd8967342e016d0be15f9d5a2
JFabianSandoval <103403098+JFabianSandoval@users.noreply.github.com>
2024-06-27 23:35:08 -0500

    Add files via upload
    Página web actualizado, este repositorio es para todo público, colaborando a la comunidad de programadores. Muchas gracias por revisarlo.
    Atte Estudiantes del curso Ing. de Software de la UPC, curso IHC y Tecnologias moviles
    
    ¿Cómo abrir la página web?
    1. Descargue el folder principal llamado AGRO H2O 2.0
    2. Una vez descargado o zipeado extraiga todo su contenido.
    3. Ingrese a la carpeta extraida e ingrese a la carpeta HTML
    4. De click al documento index
    
    
    
    Integrantes:
    Fabian Jesus Sandoval Cueto
    Nestor Alonso Rojas Tello
    Stephano Jose Espinoza Cueva
    Yeira Shari Huaman Olivos

---
+++ AGROH2O/JS/script.js
@@ -0,0 +1,33 @@
+function showServicios() {
+    var serviciosDiv = document.getElementById("ServiciosDIV");
+    if (serviciosDiv.style.display === "none") {
+        serviciosDiv.style.display = "flex";
+    } else {
+        serviciosDiv.style.display = "none";
+    }
+}
+function showContactanos() {
+    var contactanosDiv = document.getElementById("ContactanosDIV");
+    if (contactanosDiv.classList.contains("Hidden")) {
+        contactanosDiv.classList.remove("Hidden");
+    } else {
+        contactanosDiv.classList.add("Hidden");
+    }
+}
+
+function showServicios() {
+    var serviciosDiv = document.getElementById("ServiciosDIV");
+    if (serviciosDiv.style.display === "none") {
+        serviciosDiv.style.display = "flex";
+    } else {
+        serviciosDiv.style.display = "none";
+    }
+}
+function showContactanos() {
+    var contactanosDiv = document.getElementById("ContactanosDIV");
+    if (contactanosDiv.classList.contains("Hidden")) {
+        contactanosDiv.classList.remove("Hidden");
+    } else {
+        contactanosDiv.classList.add("Hidden");
+    }
+}
